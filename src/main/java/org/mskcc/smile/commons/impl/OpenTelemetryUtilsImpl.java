af476199f6cc954e29903794ad3512f4ff6c0883
Benjamin Gross <benjamin.gross@gmail.com>
2024-01-30 11:16:34 -0500

    otel b3 trace propagation support

--- src/main/java/org/mskcc/smile/commons/impl/OpenTelemetryUtilsImpl.java
+++ src/main/java/org/mskcc/smile/commons/impl/OpenTelemetryUtilsImpl.java
@@ -1,60 +1,47 @@
 package org.mskcc.smile.commons.impl;
 
-import io.grpc.Metadata;
-import io.opentelemetry.api.GlobalOpenTelemetry;
+import org.mskcc.smile.commons.OpenTelemetryUtils;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.propagation.TextMapGetter;
-import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.context.propagation.TextMapSetter;
-import org.mskcc.smile.commons.OpenTelemetryUtils;
+import io.opentelemetry.extension.trace.propagation.B3Propagator;
 import org.springframework.stereotype.Component;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nullable;
 
 @Component
 public class OpenTelemetryUtilsImpl implements OpenTelemetryUtils {
 
-    private static final TextMapPropagator textFormat =
-        GlobalOpenTelemetry.getPropagators().getTextMapPropagator();
-
-    private static final Tracer tracer =
-        GlobalOpenTelemetry.get().getTracer("org.mskcc.smile.commons.impl.OpenTelemetryImpl");
-
-    private static final TextMapGetter<Metadata> getter =
-        new TextMapGetter<Metadata>() {
+    private static final TextMapGetter<Map<String, String>> getter =
+        new TextMapGetter<Map<String, String>>() {
             @Override
-            public Iterable<String> keys(Metadata carrier) {
-                return carrier.keys();
+            public Iterable<String> keys(Map<String, String> carrier) {
+                return carrier.keySet();
             }
 
+            @Nullable
             @Override
-            public String get(Metadata carrier, String key) {
-                Metadata.Key<String> k = Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER);
-                if (carrier.containsKey(k)) {
-                    return carrier.get(k);
-                }
-                return "";
+            public String get(Map<String, String> carrier, String key) {
+                return carrier.get(key);
             }
         };
 
-    TextMapSetter<Metadata> setter =
-        (carrier, key, value) ->
-        carrier.put(Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER), value);
-
     @Override
-    public TraceMetadata getTraceMetadata() {
-        Metadata headers = new Metadata();
-        textFormat.inject(Context.current(), headers, setter);
-        String traceId = getter.get(headers, TraceMetadata.getHeaderKey());
-        return new TraceMetadata(traceId);
+    public Map<String, String> getTraceMetadata(Context context) {
+        B3Propagator b3Propagator = B3Propagator.injectingMultiHeaders();
+        TextMapSetter<Map<String, String>> setter = Map::put;
+        Map<String, String> carrier = new HashMap<>();
+        b3Propagator.inject(context, carrier, setter);
+        return carrier;
     }
 
     @Override
-    public Span getSpan(TraceMetadata traceMetadata, String spanName) {
-        Metadata headers = new Metadata();
-        headers.put(Metadata.Key.of(TraceMetadata.getHeaderKey(),
-                                    Metadata.ASCII_STRING_MARSHALLER), traceMetadata.getMetadata());
-        Context extractedContext = textFormat.extract(Context.current(), headers, getter);
-        return tracer.spanBuilder(spanName).setParent(extractedContext).startSpan();
+    public Span getSpanFromTraceMetadata(String spanName, Tracer tracer, Map<String, String> traceMetadata) {
+        B3Propagator b3Propagator = B3Propagator.injectingMultiHeaders();
+        Context propContext = b3Propagator.extract(Context.current(), traceMetadata, getter);
+        return tracer.spanBuilder(spanName).setParent(propContext).startSpan();
     }
 }
